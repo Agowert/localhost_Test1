3afed3260e15eacab1af1a6347ef504cb94a0f6c
Agowert1 <70849596+Agowert@users.noreply.github.com>
2024-07-14 22:38:03 +0300

    Revert "Delete local/templates/exam1/components/bitrix/news/News-ex/bitrix/news.detail/.default/template.php"
    This reverts commit ec7f56d2336fb3cac9799d54066b4684a6e24355.

---
+++ local/templates/exam1/components/bitrix/news/News-ex/bitrix/news.detail/.default/template.php
@@ -0,0 +1,67 @@
+<?if(!defined("B_PROLOG_INCLUDED") || B_PROLOG_INCLUDED!==true)die();
+/** @var array $arParams */
+/** @var array $arResult */
+/** @global CMain $APPLICATION */
+/** @global CUser $USER */
+/** @global CDatabase $DB */
+/** @var CBitrixComponentTemplate $this */
+/** @var string $templateName */
+/** @var string $templateFile */
+/** @var string $templateFolder */
+/** @var string $componentPath */
+/** @var CBitrixComponent $component */
+$this->setFrameMode(true);
+$img = SITE_TEMPLATE_PATH."/img/rew/no_photo.jpg"
+
+
+?>
+
+
+
+<div class="review-block">
+							<div class="review-text">
+								<div class="review-text-cont">
+							
+								<?= $arResult["DETAIL_TEXT"]?>
+								</div>
+								<div class="review-autor">
+								
+
+
+									<?= $arResult["NAME"]?>
+									<?= $arResult["DISPLAY_ACTIVE_FROM"]?> <?=GetMessage("Year")?>
+								
+								<?= $arResult["DISPLAY_PROPERTIES"]["POSITION"]["VALUE"]?> 
+								
+								<?= $arResult["DISPLAY_PROPERTIES"]["COMPANY"]["VALUE"] ?>
+								</div>
+							</div>
+
+							<?if (isset($arResult["DETAIL_PICTURE"]["SRC"])){
+	$img = $arResult["DETAIL_PICTURE"]["SRC"];}
+ ?>
+							<div style="clear: both;" class="review-img-wrap"><img src="<?=$img?>" alt=""></div>
+						</div>
+
+						
+						<?
+						if ($arResult["PROPERTIES"]["FILE"]["VALUE"] != 0){
+							?> <p>Документы:</p>  <?
+						 foreach ($arResult["PROPERTIES"]["FILE"]["VALUE"] as $File )
+						 
+{?>
+
+
+
+<?  $Item = CFile::GetFileArray($File)  ?>
+						<div  class="exam-review-item-doc"><img class="rew-doc-ico" src="<?=SITE_TEMPLATE_PATH?>/img/icons/pdf_ico_40.png"> <a href="<?=$Item["SRC"]?> "download > <?=$Item["ORIGINAL_NAME"]?> </div>
+
+						<?  
+					
+						
+					}}?>
+						
+						
+
+
+	
